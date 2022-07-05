e39cb4bf3ba995a16b4e4a8f6e16da4b0931031c
Ausakura <17104706+Ausakura@users.noreply.github.com>
2022-07-04 22:31:19 -0400

    scheduler hooks

--- src/hooks/cocos/scheduler_hook.cc
+++ src/hooks/cocos/scheduler_hook.cc
@@ -5,36 +5,28 @@
 #include <deque>
 #include <mutex>
 #include <thread>
+#include <tuple>
 
-std::deque<std::function<void(v8::Local<v8::Context>)>> g_v8_context_runners;
-std::deque<v8::Isolate*> g_v8_isolates;
-
-bool in_context_runner = false;
+std::deque<std::tuple<v8::Isolate *, std::string, std::function<void(std::string, v8::Local<v8::Context>)>>> g_v8_context_runners;
+std::deque<std::tuple<v8::Isolate *, v8::Local<v8::Value>, std::function<void(v8::Local<v8::Value>, v8::Local<v8::Context>)>>> g_v8_value_context_runners;
 
 namespace shimakaze
 {
     namespace scheduler
     {
-        void run_under_context(v8::Isolate* isolate, std::function<void(v8::Local<v8::Context>)> func)
+        void run_under_context(v8::Isolate *isolate, std::string script, std::function<void(std::string, v8::Local<v8::Context>)> func)
         {
-            g_v8_context_runners.push_back(func);
-            g_v8_isolates.push_back(isolate);
+            g_v8_context_runners.push_back(std::make_tuple(isolate, script, func));
         }
-        
-        void run_context_function(v8::Isolate* isolate, std::function<void(v8::Local<v8::Context>)> func)
+        void run_value_under_context(v8::Isolate *isolate, v8::Local<v8::Value> value, std::function<void(v8::Local<v8::Value>, v8::Local<v8::Context>)> func)
         {
-            if (in_context_runner == true) {
-                // wait a bit, we're on a seperate thread anyways xd
-                while (in_context_runner == true) {
-                    if (in_context_runner == false) {
-                        break;
-                    }
+            g_v8_value_context_runners.push_back(std::make_tuple(isolate, value, func));
+        }
 
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                }
-            }
-
+        void run_context_function(v8::Isolate *isolate, std::string script, std::function<void(std::string, v8::Local<v8::Context>)> func)
+        {
             in_context_runner = true;
+
             // create a context under this thread
             v8::Locker locker(isolate);
             v8::Isolate::Scope isolate_scope(isolate);
@@ -43,10 +35,31 @@ namespace shimakaze
             v8::Local<v8::Context> context = v8::Local<v8::Context>::New(isolate, core::g_global_context);
 
             // run function after thoroughly locking the current isolate to the thread
-            func(context);
+            func(script, context);
 
             // we're done
             v8::Unlocker unlocker(isolate);
+
+            in_context_runner = false;
+        }
+
+        void run_value_context_function(v8::Isolate *isolate, v8::Local<v8::Value> value, std::function<void(v8::Local<v8::Value>, v8::Local<v8::Context>)> func)
+        {
+            in_context_runner = true;
+
+            // create a context under this thread
+            v8::Locker locker(isolate);
+            v8::Isolate::Scope isolate_scope(isolate);
+            v8::HandleScope handle_scope(isolate);
+
+            v8::Local<v8::Context> context = v8::Local<v8::Context>::New(isolate, core::g_global_context);
+
+            // run function after thoroughly locking the current isolate to the thread
+            func(value, context);
+
+            // we're done
+            v8::Unlocker unlocker(isolate);
+
             in_context_runner = false;
         }
     }
@@ -54,8 +67,13 @@ namespace shimakaze
 
 SHIMAKAZE_CALL_ARGS(CCScheduler_update, CCScheduler *, void, float dt)
 {
+    if (shimakaze::scheduler::scheduler_started == false) {
+        shimakaze::scheduler::scheduler_started = true;
+        shimakaze::scheduler::in_context_runner = false;
+    }
 
-    if (g_v8_context_runners.empty() || in_context_runner == true) {
+    if ((g_v8_context_runners.empty() && g_v8_value_context_runners.empty()) || shimakaze::scheduler::in_context_runner == true)
+    {
         // run to the game loop if we have no contexts to run
         CCScheduler_update(self, dt);
 
@@ -65,17 +83,52 @@ SHIMAKAZE_CALL_ARGS(CCScheduler_update, CCScheduler *, void, float dt)
     // run v8 contexts
     for (auto const &func : g_v8_context_runners)
     {
+        if (shimakaze::scheduler::in_context_runner == true)
+        {
+            break;
+        }
+
+        shimakaze::scheduler::in_context_runner = true;
+
         // get current isolate
-        v8::Isolate* isolate = g_v8_isolates.front();
+        v8::Isolate *isolate = std::get<0>(func);
 
         // create a seperate thread
-        std::thread context_thread(&shimakaze::scheduler::run_context_function, isolate, func);
+        std::thread context_thread(&shimakaze::scheduler::run_context_function, isolate, std::get<1>(func), std::get<2>(func));
 
-        context_thread.join();
+        context_thread.detach();
 
         // dequeue
         g_v8_context_runners.pop_front();
-        g_v8_isolates.pop_front();
+    }
+
+    if ((g_v8_context_runners.empty() && g_v8_value_context_runners.empty()) || shimakaze::scheduler::in_context_runner == true)
+    {
+        // run to the game loop if we have no contexts to run
+        CCScheduler_update(self, dt);
+
+        return;
+    }
+
+    // run v8 contexts under a value
+    for (auto const &func : g_v8_value_context_runners)
+    {
+        if (shimakaze::scheduler::in_context_runner == true)
+        {
+            break;
+        }
+
+        shimakaze::scheduler::in_context_runner = true;
+        // get current isolate
+        v8::Isolate *isolate = std::get<0>(func);
+
+        // create a seperate thread
+        std::thread context_thread(&shimakaze::scheduler::run_value_context_function, isolate, std::get<1>(func), std::get<2>(func));
+
+        context_thread.detach();
+
+        // dequeue
+        g_v8_value_context_runners.pop_front();
     }
 
     // update scheduler
--- src/hooks/cocos/scheduler_hook.h
+++ src/hooks/cocos/scheduler_hook.h
@@ -6,8 +6,14 @@ namespace shimakaze
 {
     namespace scheduler
     {
-        void run_under_context(v8::Isolate* isolate, std::function<void(v8::Local<v8::Context>)> func);
-        void run_context_function(v8::Isolate* isolate, std::function<void(v8::Local<v8::Context>)> func);
+        void run_under_context(v8::Isolate* isolate, std::string script, std::function<void(std::string, v8::Local<v8::Context>)> func);
+        void run_value_under_context(v8::Isolate* isolate, v8::Local<v8::Value> value, std::function<void(v8::Local<v8::Value>, v8::Local<v8::Context>)> func);
+        
+        void run_context_function(v8::Isolate* isolate, std::string script,  std::function<void(std::string, v8::Local<v8::Context>)> func);
+        void run_value_context_function(v8::Isolate* isolate, v8::Local<v8::Value> value, std::function<void(v8::Local<v8::Value>, v8::Local<v8::Context>)> func);
+    
+        inline bool in_context_runner = true;
+        inline bool scheduler_started = false;
     }
 }
 
