d079780725509e72f4986b16a5bd6af2337f129f
Ausakura <17104706+Ausakura@users.noreply.github.com>
2022-07-04 21:07:01 -0400

    to v8

---
+++ src/core/v8/convert.cc
@@ -0,0 +1,42 @@
+#include "convert.h"
+
+namespace shimakaze
+{
+    namespace bind
+    {
+        v8::Local<v8::String> to_v8(v8::Isolate *isolate, const char *str)
+        {
+            return v8::String::NewFromUtf8(isolate, str, v8::NewStringType::kNormal).ToLocalChecked();
+        }
+
+        v8::Local<v8::String> to_v8(v8::Isolate *isolate, std::string str)
+        {
+            return v8::String::NewFromUtf8(isolate, str.c_str(), v8::NewStringType::kNormal).ToLocalChecked();
+        }
+
+        v8::Local<v8::Number> to_v8(v8::Isolate *isolate, double value)
+        {
+            return v8::Number::New(isolate, value);
+        }
+
+        v8::Local<v8::Number> to_v8(v8::Isolate *isolate, float value)
+        {
+            return v8::Number::New(isolate, value);
+        }
+
+        v8::Local<v8::Number> to_v8(v8::Isolate *isolate, int value)
+        {
+            return v8::Number::New(isolate, value);
+        }
+
+        v8::Local<v8::Number> to_v8(v8::Isolate *isolate, long value)
+        {
+            return v8::Number::New(isolate, value);
+        }
+
+        const char* to_cstr(const v8::String::Utf8Value& str)
+        {
+            return *str ? *str : "<unknown string>";
+        }
+    }
+}
